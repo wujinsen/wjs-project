5b2195080b45adba93cfb285fd9abb4ee1e7792f
wujinsen <wujinsen@aliyun.com>
2021-12-23 11:25:43 +0800

    commit

---
+++ xxl-job-learning/src/main/java/cn/wjs/XXLJobApplication.java
@@ -0,0 +1,11 @@
+package cn.wjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XXLJobApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XXLJobApplication.class, args);
+    }
+}
