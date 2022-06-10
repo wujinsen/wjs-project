6b0911315c5004a23542feb11f37f89889c2f816
wujinsen <2998714655@qq.com>
2022-06-10 19:04:43 +0800

    redisson分布式锁-模拟抢购

--- springboot-rabbitmq-demo/src/main/java/cn/wjs/send/listener/MoliListener.java
+++ springboot-rabbitmq-demo/src/main/java/cn/wjs/send/listener/MoliListener.java
@@ -9,8 +9,18 @@ public class MoliListener {
 
     @RabbitListener(queues = {"moliQueue"})
     public void listenMoliQueue(Message msg) throws Exception {
-        System.out.println("moli监听消息: " + msg);
+
+        System.out.println("moliQueue moli监听消息: " + msg);
         System.out.println("具体消息： " + new String(msg.getBody(), "UTF-8"));
+
+    }
+
+    //@RabbitListener(queues = {"moliQueue2"})
+    public void listenMoliQueue2(Message msg) throws Exception {
+
+        System.out.println("moliQueue2 moli监听消息: " + msg);
+        System.out.println("具体消息： " + new String(msg.getBody(), "UTF-8"));
+
     }
 
 }
--- springboot-rabbitmq-demo/src/test/cn.wjs/RabbitmqTest.java
+++ springboot-rabbitmq-demo/src/test/cn.wjs/RabbitmqTest.java
@@ -16,18 +16,33 @@ public class RabbitmqTest {
     @Resource
     private RabbitTemplate rabbitTemplate;
 
+
     @Test
     public void sendTest(){
-        String queueName = "moliQueue";
+        String routingKey = "moliKey";
         String message = "hello world 哈哈";
-        rabbitTemplate.convertAndSend("moliExchange", queueName, message);
+        String routingKey2 = "moliKey2";
+        String message2 = "hello world 2号";
+        rabbitTemplate.convertAndSend("moliExchange", routingKey, message);
 
+        rabbitTemplate.convertAndSend("moliExchange", routingKey2, message2);
     }
+
     @Test
     public void receiveTest(){
         String queueName = "moliQueue";
 
         System.out.println("消费消息: " + rabbitTemplate.receiveAndConvert(queueName));
 
+    }
+
+    @Test
+    public void sendTopicTest(){
+        String routingKey = "order.1";
+        String message = "hello world order1";
+        String routingKey2 = "order.2";
+        String message2 = "hello world order2";
+        rabbitTemplate.convertAndSend("moli_topic_exchange", routingKey, message);
+        rabbitTemplate.convertAndSend("moli_topic_exchange", routingKey2, message2);
     }
 }
