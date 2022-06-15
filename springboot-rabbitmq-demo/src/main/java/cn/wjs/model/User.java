40723bd2b51776a4a9676a676ea94c17029e6b1e
wujinsen <2998714655@qq.com>
2022-06-16 00:41:29 +0800

    redisson分布式锁-模拟抢购

--- springboot-rabbitmq-demo/src/main/java/cn/wjs/model/User.java
+++ springboot-rabbitmq-demo/src/main/java/cn/wjs/model/User.java
@@ -1,8 +1,15 @@
 package cn.wjs.model;
 
 
+import com.alibaba.fastjson.JSON;
+
 public class User {
 
+    public static void main(String[] args) {
+        String str = "{\"id\":1, \"name\":\"zhangsan\"}";
+        User user = JSON.parseObject(str, User.class);
+        System.out.println(user);
+    }
     private Integer id;
 
     private String name;
--- springboot-rabbitmq-demo/src/main/java/cn/wjs/send/listener/MoliListener.java
+++ springboot-rabbitmq-demo/src/main/java/cn/wjs/send/listener/MoliListener.java
@@ -11,29 +11,38 @@ import org.springframework.stereotype.Component;
 @Component
 public class MoliListener {
 
-    @RabbitListener(queues = {"moliQueue"})
-    public void listenMoliQueue(Message msg) throws Exception {
+//    @RabbitListener(queues = {"moliQueue"})
+//    public void listenMoliQueue(Message msg) throws Exception {
+//
+//        System.out.println("moliQueue moli监听消息: " + msg);
+//        System.out.println("具体消息： " + new String(msg.getBody(), "UTF-8"));
+//
+//    }
+//
+//    //@RabbitListener(queues = {"moliQueue2"})
+//    public void listenMoliQueue2(Message msg) throws Exception {
+//
+//        System.out.println("moliQueue2 moli监听消息: " + msg);
+//        System.out.println("具体消息： " + new String(msg.getBody(), "UTF-8"));
+//
+//    }
+//
+//    @RabbitListener(queues = {"moliQueue4"})
+//    @RabbitHandler
+//    public void listenMoliQueue3(@Payload Message msg) throws Exception {
+//        User user = JSON.parseObject(msg.getBody(), User.class);
+//        System.out.println("moliQueue3 moli监听消息: " + msg);
+//        System.out.println("具体消息: " + JSON.toJSONString(user));
+//    }
 
-        System.out.println("moliQueue moli监听消息: " + msg);
-        System.out.println("具体消息： " + new String(msg.getBody(), "UTF-8"));
-
-    }
-
-    //@RabbitListener(queues = {"moliQueue2"})
-    public void listenMoliQueue2(Message msg) throws Exception {
-
-        System.out.println("moliQueue2 moli监听消息: " + msg);
-        System.out.println("具体消息： " + new String(msg.getBody(), "UTF-8"));
-
-    }
-
-    @RabbitListener(queues = {"moliQueue3"})
+    @RabbitListener(queues = {"gift.send.room"})
     @RabbitHandler
     public void listenMoliQueue3(@Payload Message msg) throws Exception {
-        User user = JSON.parseObject(msg.getBody(), User.class);
+       // User user = JSON.parseObject(msg.getBody(), User.class);
         System.out.println("moliQueue3 moli监听消息: " + msg);
-        System.out.println("具体消息: " + JSON.toJSONString(user));
+       // System.out.println("具体消息: " + JSON.toJSONString(user));
     }
+
 
 }
 
